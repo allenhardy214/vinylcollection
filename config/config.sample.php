9d3becff5fc5d54bc790236cbc8cdd78388296bb
allen <allenhardy214@gmail.com>
2014-04-17 15:16:40 +0100

    Added example config

---
+++ config/config.sample.php
@@ -0,0 +1,12 @@
+<?
+date_default_timezone_set("Europe/London");
+
+define('DEV',true);
+
+define('DB_NAME',name);
+define('DB_USER',user);
+define('DB_PASSWORD',password);
+define('DB_HOST',host);
+define('DB_PORT',3306);
+
+
