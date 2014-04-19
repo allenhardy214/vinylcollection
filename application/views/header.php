52403ca8ffe96cbc3d4ec4944d649b94eb00d50f
Allen Hardy <allenhardy214@gmail.com>
2014-04-19 14:34:56 +0100

    Styling

--- application/views/header.php
+++ application/views/header.php
@@ -1,10 +1,10 @@
 <html>
   <head>
   <title><?=$data['title']?></title>
-  <link rel="stlyesheet" type="text/css"  href="/public/css/style.css" />
-
+  <link href="/public/css/style.css?cache=260214-1343" media="" rel="stylesheet" type="text/css"/>
   </head>
   <body>
+	  <div id="wrapper">
 	  <div id="header">
 		<h1><?=$data['title']?></h1>
 	  </div>
