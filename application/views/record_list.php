df35c26715d2da57401f8217e2100c3e5a9183d9
allen <allenhardy214@gmail.com>
2014-04-22 10:05:59 +0100

    Fleshing out a dialogue

--- application/views/record_list.php
+++ application/views/record_list.php
@@ -46,6 +46,54 @@
 <?endif;?>
 <div class="dialogues">
 
-	<div id="edit_record_dialogue" class="dialogue">Edit Record</div>
-
+	<div id="edit_record_dialogue" class="dialogue">
+    <div class="title">Edit Record</div>
+    <form id="edit_record">
+      <input type="hidden" name="record_id" value=""/>
+      <fieldset>
+        <ul>
+          <li>
+            <label for="record_title">Title</label>
+            <input type="text" name="record_title" value="" />
+          </li>
+          <li>
+            <label for="record_artist">Artist</label>
+            <input type="text" name="record_artist" value="" />
+          </li>
+          <li>
+            <label for="record_title">Label</label>
+            <input type="text" name="record_label" value="" />
+          </li>
+          <li>
+            <label for="record_size">Size</label>
+            <select name="record_size">
+            </select>
+          </li>
+          <li>
+            <label for="record_type">Type</label>
+            <select name="record_type">
+            </select>
+          </li>
+          <li>
+            <label for="record_sides">Sides</label>
+            <select name="record_sides">
+            </select>
+          </li>
+          <li>
+            <label for="record_release">Released</label>
+            <input type="text" name="record_release" class="integer minlength4" value=""/>
+          </li>
+          <li>
+            <label for="record_condition">Sides</label>
+            <select name="record_condition">
+            </select>
+          </li>
+        </ul>
+      </fieldset>
+      <div class="buttons">
+        <button type="button" class="save"><span>Save</span></button>
+        <button type="button" class="cancel"><span>Cancel</span></button>
+      </div>
+    </form>
+  </div>
 </div>
